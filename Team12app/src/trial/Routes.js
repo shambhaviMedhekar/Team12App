4c2eced26b53ceb28c5bec33b74a4b809c3d0efd
samikshakulkarni <36085626+samikshakulkarni@users.noreply.github.com>
2018-05-10 23:37:33 -0500

    Update Routes.js

--- Team12app/src/trial/Routes.js
+++ Team12app/src/trial/Routes.js
@@ -3,6 +3,7 @@ import {Router, Stack, Scene} from 'react-native-router-flux';
 import Facility from './Facility';
 import LoginTrial from './LoginTrial';
 import FacilityDetails from './FacilityDetails';
+import FacilityforUser from './FacilityforUser';
 import ResourceDetails from './ResourceDetails';
 export default class Routes extends React.Component {
   render() {
@@ -12,7 +13,7 @@ export default class Routes extends React.Component {
             <Stack key="root" hideNavBar>
                 <Scene key="login" component={LoginTrial} title="Login"/>
                 <Scene key="facility" component={Facility} title="Facility"/>
-                <Scene key="facilityforUser" component={FacilityforUser} title="facilityforUser"/>
+              <Scene key="facilityforUser" component={FacilityforUser} title="FacilityforUser"/>
                 <Scene key="facilityDetails" component={FacilityDetails} title="FacilityDetails"/>
                 <Scene key="resourceDetails" component={ResourceDetails} title="ResourceDetails"/>
             </Stack>
